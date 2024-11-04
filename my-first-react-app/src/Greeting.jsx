96acf9fefa001744ee623c4143e71dc59f1fc8c6
Mr.Marshal <marshalrock11@gmail.com>
2024-11-04 15:50:19 +1000

    Testing react components

---
+++ my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,14 @@
+function Demo() {
+  return (
+    <div>
+      <img src="/demographics.png" alt="elderly demographics"></img>
+      <form>
+        <label htmlFor='random-input'>enter age here:</label>
+        <input id='random-input' type='number'></input>
+      </form>
+      <h1>Elderly demographics and img test</h1>
+    </div>
+  );
+}
+
+export default Demo;
