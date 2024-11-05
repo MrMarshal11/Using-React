56856710f04ac0c9a87f595a01cdf5bd89ea4ffc
Mr.Marshal <marshalrock11@gmail.com>
2024-11-05 20:10:33 +1000

    Stupid functionality but...
    Just testing out react states and rendering principles.

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './Greeting.css'
 
 function Demo() {
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <div>
       <img src="/demographics.png" alt="elderly demographics"></img>
       <form>
         <label htmlFor='random-input'>enter age here:</label>
-        <input id='random-input' type='number'></input>
+        <input 
+        id='random-input' 
+        type='number' 
+        value={inputValue}
+        onChange={(event) => (inputValue === -1 ? setInputValue(1) : setInputValue(event.target.value - 1))}
+        ></input>
       </form>
       <h1>Elderly demographics and img test</h1>
     </div>
