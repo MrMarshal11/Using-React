fb4b50b6da1ac01e3fe2a1dcc24c1c6ff5335cea
Mr.Marshal <marshalrock11@gmail.com>
2024-11-07 07:51:21 +1000

    Add functionality and temporary forms

--- Project - CV Application/src/form.jsx
+++ Project - CV Application/src/form.jsx
@@ -1,9 +1,7 @@
 // A section to add general information like name, email and phone number.
-// A section to add your educational experience (school name, title of study and date of study)
-// A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
 
 import { useState } from "react";
-import './form.css'
+import './Form.css'
 
 function Form() {
   return (
@@ -23,16 +21,6 @@ function Form() {
             <label>Phone Number: </label>
             <input />
         </fieldset>
-
-        {/* Add Educational Experience? h3 and Add Professional Experience h3
-
-        and a button underneath that disappears when clicked (saying yes for now)
-
-        and a save button shows up instead along with the formfield 
-
-        and a remove button also shows up and disappears when clicked along with the formfield that persists unless clicked
-        
-         */}
       </form>
     </div>
   );
--- Project - CV Application/src/main.jsx
+++ Project - CV Application/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Form from './form.jsx'
+import Experience from './Experience.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Form />
+    <Experience />
   </StrictMode>,
 )
