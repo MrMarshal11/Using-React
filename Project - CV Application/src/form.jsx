e892ff1b2adf80f4b3f327d413e856dcbebcc55a
Mr.Marshal <marshalrock11@gmail.com>
2024-11-07 03:41:08 +1000

    Starting template

---
+++ Project - CV Application/src/form.jsx
@@ -0,0 +1,18 @@
+// A section to add general information like name, email and phone number.
+// A section to add your educational experience (school name, title of study and date of study)
+// A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
+
+import { useState } from "react";
+
+function Form() {
+    return (
+        <div>
+            <form>
+                <label>Name</label>
+                <input />
+            </form>
+        </div>
+    )
+}
+
+export default Form;
