86ffc29113f072485a39ffe825bc82b6e22b9b5c
Mr.Marshal <marshalrock11@gmail.com>
2024-11-07 04:04:41 +1000

    Current plan for next steps

--- Project - CV Application/src/form.jsx
+++ Project - CV Application/src/form.jsx
@@ -3,16 +3,39 @@
 // A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
 
 import { useState } from "react";
+import './form.css'
 
 function Form() {
-    return (
-        <div>
-            <form>
-                <label>Name</label>
-                <input />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form className="form">
+        <fieldset>
+            <label>Full Name: </label>
+            <input />
+        </fieldset>
+
+        <fieldset>
+            <label>Email: </label>
+            <input type="email"/>
+        </fieldset>
+
+        <fieldset>
+            <label>Phone Number: </label>
+            <input />
+        </fieldset>
+
+        {/* Add Educational Experience? h3 and Add Professional Experience h3
+
+        and a button underneath that disappears when clicked (saying yes for now)
+
+        and a save button shows up instead along with the formfield 
+
+        and a remove button also shows up and disappears when clicked along with the formfield that persists unless clicked
+        
+         */}
+      </form>
+    </div>
+  );
 }
 
 export default Form;
