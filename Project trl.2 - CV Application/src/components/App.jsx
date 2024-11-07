fa8a73cbadfee32fa3a3be0b503856dc18e34f88
Mr.Marshal <marshalrock11@gmail.com>
2024-11-08 09:12:47 +1000

    Basic submit functionality operational

--- Project trl.2 - CV Application/src/components/App.jsx
+++ Project trl.2 - CV Application/src/components/App.jsx
@@ -6,58 +6,68 @@ import EducationalForm from "./EducationForm";
 
 function App() {
   // PersistentForm
-  const [fullName, setFullName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phoneNumber, setPhoneNumber] = useState('')
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const updateFullName = (e) => {
     setFullName(e.target.value);
-  }
-  
+  };
+
   const updateEmail = (e) => {
     setEmail(e.target.value);
-  }
-  
+  };
+
   const updatePhoneNumber = (e) => {
     setPhoneNumber(e.target.value);
-  }
+  };
 
   // EducationalForm
   const [educationalExperience, setEducationalExperience] = useState([
-    {schoolName: 'Griffith University Gold Coast',
-      titleOfStudy: 'Bachelor of Biomedical Science',
-      dateOfStudy: '11 / 11 / 2003'
-    }
-  ])
+    {
+      id: crypto.randomUUID(),
+      schoolName: "Griffith University Gold Coast",
+      titleOfStudy: "Bachelor of Biomedical Science",
+      dateOfStudy: "11 / 11 / 2003",
+    },
+  ]);
 
-  const [schoolName, setSchoolName] = useState('')
-  const [titleOfStudy, setTitleOfStudy] = useState('')
-  const [dateOfStudy, setDateOfStudy] = useState('')
+  const [schoolName, setSchoolName] = useState("");
+  const [titleOfStudy, setTitleOfStudy] = useState("");
+  const [dateOfStudy, setDateOfStudy] = useState("");
 
   const updateSchoolName = (e) => {
     setSchoolName(e.target.value);
-  }
+  };
 
   const updateTitleOfStudy = (e) => {
     setTitleOfStudy(e.target.value);
-  }
+  };
 
   const updateDateOfStudy = (e) => {
     setDateOfStudy(e.target.value);
-  }
+  };
 
   // On button submit, update educational form
-  const submitEducationalExperience = (newData) => {
-    setEducationalExperience(educationalExperience => [...educationalExperience, newData]);
-  }
+  const submitEducationalExperience = (e) => {
+    e.preventDefault(); 
+
+    const newExperience = {
+      id: crypto.randomUUID(),
+      schoolName: schoolName,
+      titleOfStudy: titleOfStudy,
+      dateOfStudy: dateOfStudy
+    }
+
+    setEducationalExperience((array) => [...array, newExperience,]);
+  };
 
   // update on input change
   // when submit, push new object into educational experience
 
   // figure out how to do the one below:
 
-
-  // after thats done, send it to CV and then map educationalForm 
+  // after thats done, send it to CV and then map educationalForm
   // display results in a new div
 
   // another thing to fix is to make the new form submits into objects not arrays
@@ -65,29 +75,26 @@ function App() {
   return (
     <>
       <div className="left">
-        <Form 
-        updateFullName={updateFullName}
-        updateEmail={updateEmail}
-        updatePhoneNumber={updatePhoneNumber}
+        <Form
+          updateFullName={updateFullName}
+          updateEmail={updateEmail}
+          updatePhoneNumber={updatePhoneNumber}
         />
-        
-        <EducationalForm 
-        updateSchoolName={updateSchoolName}
-        updateTitleOfStudy={updateTitleOfStudy}
-        updateDateOfStudy={updateDateOfStudy}
-        submitEducationalExperience={submitEducationalExperience}
+
+        <EducationalForm
+          updateSchoolName={updateSchoolName}
+          updateTitleOfStudy={updateTitleOfStudy}
+          updateDateOfStudy={updateDateOfStudy}
+          submitEducationalExperience={submitEducationalExperience}
         />
       </div>
       <div className="right">
-        <CV 
-        fullName={fullName}
-        email={email}
-        phoneNumber={phoneNumber}
-
-        educationalExperience={educationalExperience}
-        dateOfStudy={dateOfStudy}
-        schoolName={schoolName}
-        titleOfStudy={titleOfStudy}        
+        <CV
+          fullName={fullName}
+          email={email}
+          phoneNumber={phoneNumber}
+          
+          educationalExperience={educationalExperience}
         />
       </div>
     </>
--- Project trl.2 - CV Application/src/components/CV.jsx
+++ Project trl.2 - CV Application/src/components/CV.jsx
@@ -1,30 +1,30 @@
 import "../styles/CV.css";
 
-function CV(
-  {fullName, 
-    email, 
-    phoneNumber,
+function CV({
+  fullName,
+  email,
+  phoneNumber,
 
-    educationalExperience
-  }) {
+  educationalExperience,
+}) {
   return (
     <>
-    <div>
-      <h2>Name: {fullName}</h2>
-      <h3>Email: {email}</h3>
-      <h3>Phone Number: {phoneNumber}</h3>
-    </div>
+      <div>
+        <h2>Name: {fullName}</h2>
+        <h3>Email: {email}</h3>
+        <h3>Phone Number: {phoneNumber}</h3>
+      </div>
 
-    {educationalExperience.map(arr => {
-      return (
-        <div>
-          <h2>Temp. Educational Experience Title</h2>
-          <h3>{arr.schoolName}</h3>
-          <h3>{arr.titleOfStudy}</h3>
-          <h3>{arr.dateOfStudy}</h3>
-        </div>
-      )
-    }) }
+      {educationalExperience.map((arr) => {
+        return (
+          <div key={arr.id}>
+            <h2>Temp. Educational Experience Title</h2>
+            <h3>{arr.schoolName}</h3>
+            <h3>{arr.titleOfStudy}</h3>
+            <h3>{arr.dateOfStudy}</h3>
+          </div>
+        );
+      })}
     </>
   );
 }
