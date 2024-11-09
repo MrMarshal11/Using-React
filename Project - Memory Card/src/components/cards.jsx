aadc2a7b39edde79381823d42359e2ae866d6160
Mr.Marshal <marshalrock11@gmail.com>
2024-11-09 14:46:27 +1000

    Added 1 sprite img, next is to add more than 1

--- Project - Memory Card/src/components/cards.jsx
+++ Project - Memory Card/src/components/cards.jsx
@@ -1,23 +1,32 @@
 import PropTypes from 'prop-types';
-
-// redo cards.jsx (without gpt help and do it on your own now)
-
-// on render each card should be assigned a random value
-// and on click each card's value should be changed randomly again
-// the grid should be assorted by the card's value via ascending order
-
-// the state should be stored in an array containing each card's index
-// when a card is clicked, the index gets added in the array
-// game loss when an index is added to the array that is already there
-// game win when 12 indexes are in the array.
+import { useState } from 'react';
 
 function Cards({increaseCount, updateCardsClicked}) {
   // Create list of cards and sort randomly
   const cardsValue = [];
 
+  const [pokemonImgs, setPokemonImgs] = useState('')
+
+  const insertPokemonImgs = (imgValue) => {
+    setPokemonImgs(imgValue)
+  }
+
+  async function getData() {
+    try {
+    const urls = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    const data = await urls.json();
+    insertPokemonImgs(data.sprites.front_default);
+    }
+    catch {
+      console.log('error');
+    }
+  }
+
+  getData();
+
   for (let i = 0; i < 12; i++) {
     const newCardValue = Math.random();
-    cardsValue.push({randomness: newCardValue, id: i});
+    cardsValue.push({randomness: newCardValue, id: i, img: pokemonImgs});
   }
 
   cardsValue.sort((a, b) => a.randomness - b.randomness)
@@ -32,7 +41,7 @@ function Cards({increaseCount, updateCardsClicked}) {
             key={crypto.randomUUID()} 
             onClick={() => {increaseCount(); updateCardsClicked(card.id);}}
             >
-              <div className="imgs"></div>
+              <img className="imgs" src={card.img} />
               <p className="names">{card.id}</p>
             </div>
           );
