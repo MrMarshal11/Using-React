255eda9a214d9af6692706896db5af032ae9a27e
Mr.Marshal <marshalrock11@gmail.com>
2024-11-11 10:14:04 +1000

    2 different trials

--- Project - Memory Card/src/tests/App.test.jsx
+++ Project - Memory Card/src/tests/App.test.jsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import App from '../components/App';
@@ -12,3 +12,13 @@ describe('App', () => {
     // check if App components renders headline
   });
 });
+
+describe('something truthy and falsy', () => {
+    it('true to be true', () => {
+      expect(true).toBe(true);
+    });
+  
+    it('false to be false', () => {
+      expect(false).toBe(false);
+    });
+  });
